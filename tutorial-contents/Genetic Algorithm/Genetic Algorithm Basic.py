9a9c703580553c84ba3d9ef5aba788e320afe301
Morvan <morvanzhou@hotmail.com>
2023-11-27 02:28:32 +0800

    Update Genetic Algorithm Basic.py
    update numpy

--- tutorial-contents/Genetic Algorithm/Genetic Algorithm Basic.py
+++ tutorial-contents/Genetic Algorithm/Genetic Algorithm Basic.py
@@ -34,7 +34,7 @@ def select(pop, fitness):    # nature selection wrt pop's fitness
 def crossover(parent, pop):     # mating process (genes crossover)
     if np.random.rand() < CROSS_RATE:
         i_ = np.random.randint(0, POP_SIZE, size=1)                             # select another individual from pop
-        cross_points = np.random.randint(0, 2, size=DNA_SIZE).astype(np.bool)   # choose crossover points
+        cross_points = np.random.randint(0, 2, size=DNA_SIZE).astype(np.bool_)   # choose crossover points
         parent[cross_points] = pop[i_, cross_points]                            # mating and produce one child
     return parent
 
